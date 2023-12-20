3d26c18f30f26bd2ab3ff362b57cee7e715ee6c8
marco76tv <marco.sottana@gmail.com>
2023-12-20 16:04:55 +0100

    first

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+const dotenvExpand = require('dotenv-expand');
+dotenvExpand(
+	require('dotenv').config({ path: '../../.env' /*, debug: true*/ }),
+);
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+	build: {
+		//outDir: '../../../public_html/build/ewall',
+		outDir: './Resources/dist',
+		emptyOutDir: false,
+		manifest: true,
+		rollupOptions: {
+			output: {
+				entryFileNames: `assets/[name].js`,
+				chunkFileNames: `assets/[name].js`,
+				assetFileNames: `assets/[name].[ext]`
+			}
+		}
+	},
+	plugins: [
+		laravel({
+			publicDirectory: '../../../public_html/',
+			buildDirectory: 'assets/ewall',
+			//buildDirectory: 'ewall',
+			input: [
+				//__dirname + '/Resources/sass/app.scss',
+				//__dirname + '/Resources/scss/app-mix.scss',
+                __dirname + '/Resources/css/app.css',
+				__dirname + '/Resources/js/app.js',
+				//__dirname + '/Resources/css/filament/admin/theme.scss'
+			],
+			refresh: true,
+		}),
+	],
+});
