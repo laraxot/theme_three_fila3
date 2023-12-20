a9f8a3c5c770834c52d819eb4a74d7ceca904272
marco76tv <marco.sottana@gmail.com>
2023-12-20 11:03:31 +0100

    first

---
+++ Resources/lang/it/mail.php
@@ -0,0 +1,12 @@
+<?php
+return
+    [
+        'shop' => 'Negozio',
+        'thankyou'=> 'Grazie Per Il Tuo Ordine!',
+        'subtitle'=> 'Il Tuo Ordine Presto Sarà Da Te! BUON APPETITO!',
+        'order'=> 'Stiamo Servendo L\'Ordine. Numero',
+        'total'=> 'Totale',
+        'address'=> 'Indirizzo Ordine',
+        'date'=> 'Data e Ora Di Consegna',
+        'unsubscribe'=> 'Se non hai creato un account usando questo indirizzo email, perfavore ignorala o disicriviti'
+    ];
