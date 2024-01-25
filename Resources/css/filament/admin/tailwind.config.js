a670d520ee62dc3f849335db5358b8f536f6de88
marco76tv <marco.sottana@gmail.com>
2024-01-25 18:47:23 +0100

    wip

--- Resources/css/filament/admin/tailwind.config.js
+++ Resources/css/filament/admin/tailwind.config.js
@@ -6,5 +6,14 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './vendor/awcodes/filament-curator/resources/**/*.blade.php',
+        './app/Filament/**/*.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 }
